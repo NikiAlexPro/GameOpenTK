8738111220abb411237b4cb70e9ed82dbe2e29d9
Nik <NikiAlexPro@gmail.com>
2020-09-17 22:42:10 +0300

    Version 1.0

---
+++ OpenTKGame/Game.cs
@@ -0,0 +1,48 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.ES10;
+using OpenTK.Input;
+using System;
+
+namespace OpenTKGame
+{
+    class Game : GameWindow
+    {
+        public Game(int width, int height, string title) :
+            base(width, height, GraphicsMode.Default, title)
+        { }
+
+        
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            KeyboardState input = Keyboard.GetState();
+            if(input.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            
+            //GL.ClearColor(0.2f, 0.3f, 03f, 1.0f);
+            base.OnLoad(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            //GL.Clear((int)ClearBufferMask.ColorBufferBit);
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            //GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+    }
+}
---
+++ OpenTKGame/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTKGame
+{
+    class Program
+    {
+        public static void Main()
+        {
+            using (Game game = new Game(800, 600, "My New Game"))
+            {
+                game.Run(60.0);
+            }
+
+            
+        }
+
+        
+        
+    }
+}
